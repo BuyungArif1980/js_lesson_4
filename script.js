cc11e4a3ed6c20f49504598a54dcd7052735deca
yungbow <buyungarif.budpar@gmail.com>
2020-04-20 20:24:47 +0700

    Lesson 7. Method (1)

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 class Animal {
-  // Tambahkan argument「name」dan「age」
   constructor(name, age) {
-    // Gantikan nilai string "Leo" dengan nilai milik argument name
     this.name = name;
-
-    // Gantikan「3」dengan nilai milik argument age
     this.age = age;
+  }
+
+  // Tambahkan method greet
+  greet() {
+    console.log("Halo");
   }
 }
 
-// Teruskan argument「"Mocha"」「8」ke constant animal dibawah
-const animal = new Animal("Mocha", 8);
+const animal = new Animal("Leo", 3);
 
 console.log(`Nama: ${animal.name}`);
 console.log(`Usia: ${animal.age}`);
+
+// Panggil method greet milik animal instance
+animal.greet();
