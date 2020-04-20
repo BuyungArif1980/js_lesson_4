d5d85006d6641ca1ea94e373a620f382338816a3
yungbow <buyungarif.budpar@gmail.com>
2020-04-20 19:12:44 +0700

    Lesson 3. Membuat Instance

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-// Definisikan class Animal
 class Animal {
 }
+
+// Tetapkan instance class Animal ke constant animal
+const animal = new Animal();
+
+// Tampilkan nilai milik constant animal
+console.log(animal);
