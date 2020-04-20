83456070213bb842e251f2e7ae05d37a4b435b3d
yungbow <buyungarif.budpar@gmail.com>
2020-04-20 20:09:11 +0700

    Lesson 5. Constructor (2)

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 class Animal {
-  // Tambahkan constructor
   constructor() {
-    console.log("Membuat instance baru");
+    // Tetapkan nilai string「Leo」ke property name
+    this.name = "Leo";
+
+    // Tetapkan nilai「3」ke property age
+    this.age = 3;
   }
 }
 
 const animal = new Animal();
+
+// Print「Nama: ____」
+console.log(`Nama: ${animal.name}`);
+
+// Print「Usia: __」
+console.log(`Usia: ${animal.age}`);
