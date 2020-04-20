c584245f812ec5d929f2044c50f455601ac7e447
yungbow <buyungarif.budpar@gmail.com>
2020-04-20 21:53:54 +0700

    Lesson 14. Overriding (2)

--- script.js
+++ script.js
@@ -16,12 +16,19 @@ class Animal {
 }
 
 class Dog extends Animal {
-  // Tambahkan method info
+  // Tambahkan constructor
+  constructor(name, age, breed) {
+    super(name, age);
+    this.breed = breed;
+  }
+
   info() {
     this.greet();
     console.log(`Nama saya adalah ${this.name}`);
-    console.log(`Saya berusia ${this.age} tahun`);
+    // Print 「Saya adalah seekor ____」
+    console.log(`Saya adalah seekor ${this.breed}`);
 
+    console.log(`Saya berusia ${this.age} tahun`);
     const humanAge = this.getHumanAge();
     console.log(`Saya berusia ${humanAge} tahun dalam umur manusia`);
   }
@@ -31,5 +38,6 @@ class Dog extends Animal {
   }
 }
 
-const dog = new Dog("Leo", 4);
+// Tetapkan "Chihuahua" sebagai nilai argument
+const dog = new Dog("Leo", 4, "Chihuahua");
 dog.info();
